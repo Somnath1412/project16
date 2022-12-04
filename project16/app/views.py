2539adfd5440b203088879a2fea9255a66758d98
singh somnath <114848190+Somnath1412@users.noreply.github.com>
2022-12-04 16:41:44 +0530

    Add files via upload

---
+++ project16/app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+def somnath(request):
+    return render(request,'somnath.html',)
+
+def singh(request):
+    return render(request,'singh.html',)
